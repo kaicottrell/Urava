615c19a6ce3a6cabf784508236a8a49700374292
Kai Cottrell <72996606+kaicottrell@users.noreply.github.com>
2024-09-23 16:54:24 -0600

    mongo configuration

--- DesiredSkillsTracker.Server/Program.cs
+++ DesiredSkillsTracker.Server/Program.cs
@@ -1,6 +1,27 @@
+using DesiredSkillsTracker.Server.Data;
+using DesiredSkillsTracker.Server.Documents;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Binds the value from the AppSettings into the MongoDbSettings instance
+var mongoDbSConfig = builder.Configuration.GetSection(nameof(MongoDbSettings));
+var mongoDbSettings = mongoDbSConfig.Get<MongoDbSettings>();
+
+builder.Services.Configure<IMongoDbSettings>(mongoDbSConfig);
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>(
+        mongoDbSettings.ConnectionString, mongoDbSettings.DatabaseName
+    );
+
+builder.Services.AddSingleton<IMongoDbSettings>(sp =>
+    sp.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+
+builder.Services.AddScoped(typeof(IMongoRepository<>), typeof(MongoRepository<>));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -28,3 +49,16 @@ app.MapControllers();
 app.MapFallbackToFile("/index.html");
 
 app.Run();
+
+
+public interface IMongoDbSettings
+{
+    string DatabaseName { get; set; }
+    string ConnectionString { get; set; }
+}
+
+public class MongoDbSettings : IMongoDbSettings
+{
+    public string DatabaseName { get; set; }
+    public string ConnectionString { get; set; }
+}
