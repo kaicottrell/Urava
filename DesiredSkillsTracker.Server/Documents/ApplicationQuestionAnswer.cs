494328e4c5f6192efc97924fee6bfbd286bfa0eb
Kai Cottrell <72996606+kaicottrell@users.noreply.github.com>
2024-09-23 20:33:11 -0600

    Add ApplicationQuestionAnswer model

---
+++ DesiredSkillsTracker.Server/Documents/ApplicationQuestionAnswer.cs
@@ -0,0 +1,21 @@
+﻿using MongoDbGenericRepository.Attributes;
+
+namespace Urava.Server.Documents
+{
+    /// <summary>
+    /// Saves commonly asked questions and answers for job applications.
+    /// </summary>
+    [CollectionName("ApplicationQuestionAnswers")]
+    public class ApplicationQuestionAnswer : Document
+    {
+        /// <summary>
+        /// The question being asked.
+        /// </summary>
+        public string Question { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The answer to the question.
+        /// </summary>
+        public string Answer { get; set; } = string.Empty;
+    }
+}
