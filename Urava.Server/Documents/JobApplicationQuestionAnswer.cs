13319285f01412eda88a17c0cd637cbaf7db2d28
Dylan Cottrell <dylancottrell9@gmail.com>
2024-10-03 21:02:17 -0600

    Added interview QA and Application QA classes representing documents

---
+++ Urava.Server/Documents/JobApplicationQuestionAnswer.cs
@@ -0,0 +1,13 @@
+﻿using MongoDbGenericRepository.Attributes;
+
+namespace Urava.Server.Documents
+{
+   // [CollectionName("JobApplicationQuestionAnswers")]
+    public class JobApplicationQuestionAnswer : Document
+    {
+        public string Question { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;
+
+        // TODO: Add user ID
+    }
+}
