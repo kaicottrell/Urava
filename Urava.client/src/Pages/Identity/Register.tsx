4dd3c9fac2631f1c71a0b72b68bb5e308dc96c4b
Dylan Cottrell <dylancottrell9@gmail.com>
2024-10-10 16:13:09 -0600

    Styled Register.tsx to be consistant with Login style, and added first and last name text boxes.

--- Urava.client/src/Pages/Identity/Register.tsx
+++ Urava.client/src/Pages/Identity/Register.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Logo from '/assets/images/LogoV1.png';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
 
 
 function Register() {
@@ -68,51 +77,87 @@ function Register() {
     };
 
     return (
-        <div className="containerbox">
-            <h3>Register</h3>
+        <div className="">
+            <Row className="d-flex justify-content-center my-4">
+                <Col className="text-center" xs="auto" >
+                    <Image className="w-50" src={Logo} />
+                </Col>
+            </Row>
 
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                </div><div>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={email}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label></div><div>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="confirmPassword">Confirm Password:</label></div><div>
-                    <input
-                        type="password"
-                        id="confirmPassword"
-                        name="confirmPassword"
-                        value={confirmPassword}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <button type="submit">Register</button>
+            <Row className="d-flex justify-content-center">
+                <Col s={12} md={6} lg={4} xl={3}>
+                    <Container className="w-100 containerbox bg-primary">
+                        <h3 className="text-center"> Register </h3>
+                        <Form onSubmit={handleSubmit}>
+                            <Row>
+                                <Col s={12} md={6}>
+                                    <Form.Group className="mb-3" controlId="firstName">
+                                        <Form.Label htmlFor="firstName">First Name:</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="firstName"
+                                            name="firstName"
+                                            /* value={firstName}*/
+                                            onChange={handleChange}
+                                        />
+                                    </Form.Group>
+                                </Col>
+                                <Col s={12} md={6}>
+                                    <Form.Group className="mb-3" controlId="lastName">
+                                        <Form.Label htmlFor="lastName">Last Name:</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="lastName"
+                                            name="lastName"
+                                            /*   value={lastName}*/
+                                            onChange={handleChange}
+                                        />
+                                    </Form.Group>
+                                </Col>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label htmlFor="email">Email:</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        value={email}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Group>
 
-                </div>
-                <div>
-                    <button onClick={handleLoginClick}>Go to Login</button>
-                </div>
-            </form>
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label htmlFor="password">Password:</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        id="password"
+                                        name="password"
+                                        value={password}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Group>
 
-            {error && <p className="error">{error}</p>}
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label htmlFor="confirmPassword">Confirm Password:</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        id="confirmPassword"
+                                        name="confirmPassword"
+                                        value={confirmPassword}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Group>
+                            </Row>
+                            <div className="text-center">
+                                <Button className="me-2" variant="secondary" type="submit">Register</Button>
+                                <Button className="ms-2" variant="outline-dark" onClick={handleLoginClick}>Go to Login</Button>
+                            </div>
+                        </Form>
+
+                        {error && <p className="error text-center text-danger mt-3">{error}</p>}
+
+                    </Container>
+                </Col>
+            </Row>
         </div>
     );
 }
