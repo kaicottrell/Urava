5fd82e7f9734a16c799a6b4a5265d59ea9b1206d
Dylan Cottrell <dylancottrell9@gmail.com>
2024-10-15 12:41:25 -0600

    Added reference Controller and class

---
+++ Urava.Server/Controllers/ReferenceController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Urava.Server.Documents;
+using Urava.Server.Interfaces;
+using Urava.Server.Repository;
+
+namespace Urava.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ReferenceController : ControllerBase
+    {
+        private readonly IRepository<Reference> _referenceRepo;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ReferenceController(IRepository<Reference> referenceRepo, UserManager<ApplicationUser> userManager)
+        {
+            _referenceRepo = referenceRepo;
+            _userManager = userManager;
+        }
+        /// <summary>
+        /// Takes a reference from a form on the front end passed in as type reference, then it adds the reference / saves it to the database.
+        /// </summary>
+        /// <param name="reference"></param>
+        /// <returns></returns>
+        /// 
+        [HttpPost]
+        public IActionResult AddReference([FromBody] Reference reference)
+        {
+            var userId = _userManager.GetUserId(User);
+            //save in the database
+            reference.UserId = new ObjectId(userId);
+            _referenceRepo.Add(reference);
+            _referenceRepo.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Urava.Server/Documents/Reference.cs
@@ -0,0 +1,25 @@
+﻿using Urava.Server.Documents;
+using MongoDbGenericRepository.Attributes;
+using MongoDB.Bson;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Urava.Server.Documents
+{
+    public class Reference : Document
+    {
+        [JsonIgnore]
+        public ObjectId UserId { get; set; }
+
+        public string Email { get; set; } = "No Email Found";
+
+        public string FullName { get; set; } = "No Name Found";
+        [Phone] 
+        public string PhoneNumber { get; set; } = "No Phone Number Found";
+
+        public string CurrentTitle { get; set; } = "No Current Title Found";
+
+
+        public string RelationshipSummary { get; set; } = "No Relationship Summary Found";
+    }
+}
